339e651f6e5c01b38a1c644f55eb07c376c30cb0
Tadeu Ferreira de Sousa Junior <tadeuferreirajr@gmail.com>
2016-10-15 20:26:14 -0300

    Subindo todos os projetos

---
+++ Dados/app/src/main/java/com/example/tadeu_000/dados/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.tadeu_000.dados;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends ListActivity  implements android.view.View.OnClickListener{
+
+    Button btnAdd,btnGetAll;
+    TextView student_Id;
+
+    @Override
+    public void onClick(View view) {
+        if (view== findViewById(R.id.btnGetAll)){
+
+            StudentRepo repo = new StudentRepo(this);
+
+            ArrayList<HashMap<String, String>> studentList =  repo.getStudentList();
+            if(studentList.size()!=0) {
+                ListView lv = getListView();
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
+                        student_Id = (TextView) view.findViewById(R.id.student_Id);
+                        String studentId = student_Id.getText().toString();
+                        Intent objIndent = new Intent(getApplicationContext(),StudentDetail.class);
+                        objIndent.putExtra("student_Id", Integer.parseInt( studentId));
+                        startActivity(objIndent);
+                    }
+                });
+                ListAdapter adapter = new SimpleAdapter( MainActivity.this,studentList, R.layout.view_student_entry, new String[] { "id","name"}, new int[] {R.id.student_Id, R.id.student_name});
+                setListAdapter(adapter);
+            }else{
+                Toast.makeText(this,"No data!",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGetAll = (Button) findViewById(R.id.btnGetAll);
+        btnGetAll.setOnClickListener(this);
+    }
+}
