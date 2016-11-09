5a16e11fb420fd18bd643a7449eb37b9243eecda
Tadeu Ferreira de Sousa Júnior <Tadeu Ferreira de Sousa Jnior>
2016-11-09 17:57:49 -0200

    Novos indicadores do Uol Economia

--- LineChart/app/src/main/java/com/example/tadeu_000/linechart/IndicadoresActivity.java
+++ LineChart/app/src/main/java/com/example/tadeu_000/linechart/IndicadoresActivity.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 public class IndicadoresActivity extends AppCompatActivity {
 
     String webpage = null;
-
     String Bovespapp = null;
     String Bovespa = null;
     String Dolarpp = null;
@@ -23,6 +22,10 @@ public class IndicadoresActivity extends AppCompatActivity {
     String SalarioMin = null;
     String CDI = null;
     String SELIC = null;
+    String DataRefDesemp = null;
+    String Desemp = null;
+    String DataRefDividaGov = null;
+    String DividaGov = null;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,13 +77,32 @@ public class IndicadoresActivity extends AppCompatActivity {
                 SELIC = arr[i + 1];
             }
 
-            //if (arr[i].equals("SELIC")) {
-            //    System.out.println(arr[i]);
-            //   System.out.println(arr[i + 1]);
-            //   System.out.println(arr[i + 2]);
-            //   System.out.println(arr[i + 3]);
-            //}
+            if (arr[i].equals("Desemprego")) {
+                DataRefDesemp = arr[i+4];
+                Desemp = arr[i+6] + " %";
+            }
+
+            if (arr[i].equals("Dívida") && arr[i+1].equals("Bruta")) {
+                DataRefDividaGov = arr[i+8];
+                DividaGov = arr[i+10] + " (milhões)";
+            }
+
+            if (arr[i].equals("Dívida") && arr[i+1].equals("Bruta")) {
+                System.out.println(arr[i]);
+                System.out.println(arr[i + 1]);
+                System.out.println(arr[i + 2]);
+                System.out.println(arr[i + 3]);
+                System.out.println(arr[i + 4]);
+                System.out.println(arr[i + 5]);
+                System.out.println(arr[i + 6]);
+                System.out.println(arr[i + 7]);
+                System.out.println(arr[i + 8]);
+                System.out.println(arr[i + 9]);
+                System.out.println(arr[i + 10]);
+            }
+
         }
+
 
         TextView BovespappTextView = (TextView) findViewById(R.id.textViewBovespapp);
         TextView BovespaTextView = (TextView) findViewById(R.id.textViewBovespa);
@@ -88,6 +110,10 @@ public class IndicadoresActivity extends AppCompatActivity {
         TextView SalarioMinTextView = (TextView) findViewById(R.id.textViewSalarioMin);
         TextView CDITextView = (TextView) findViewById(R.id.textViewCDI);
         TextView SELICTextView = (TextView) findViewById(R.id.textViewSELIC);
+        TextView DataRefDesempTextView = (TextView) findViewById(R.id.textViewDataRefDesemp);
+        TextView DesempTextView = (TextView) findViewById(R.id.textViewDesemp);
+        TextView DataRefDividaGovTextView = (TextView) findViewById(R.id.textViewDataRefDividaGov);
+        TextView DividaGovTextView = (TextView) findViewById(R.id.textViewDividaGov);
 
         BovespappTextView.setText(Bovespapp);
         BovespaTextView.setText(Bovespa);
@@ -95,19 +121,29 @@ public class IndicadoresActivity extends AppCompatActivity {
         SalarioMinTextView.setText(SalarioMin);
         CDITextView.setText(CDI);
         SELICTextView.setText(SELIC);
+        DataRefDesempTextView.setText(DataRefDesemp);
+        DesempTextView.setText(Desemp);
+        DataRefDividaGovTextView.setText(DataRefDividaGov);
+        DividaGovTextView.setText(DividaGov);
 
     }// On Create
 }// Class
 
     class ParsePage extends AsyncTask<String,Void,String> {
+
         @Override
         protected String doInBackground(String... arg0) {
             org.jsoup.nodes.Document doc;
+            org.jsoup.nodes.Document doc2;
             String resultado = "";
             try {
                 doc = Jsoup.connect("http://economia.uol.com.br/cotacoes/indices-economicos/").get();
-                resultado = doc.text();
+                doc2 = Jsoup.connect("http://www.bcb.gov.br/pec/sdds/port/sddsp.htm?perfil=1").get();
 
+                //resultado = doc.text();
+               resultado = doc.text() + doc2.text();
+                //Log.d("eita",doc.text());
+                //System.out.println(doc.text());
             } catch (IOException e) {
                 e.printStackTrace();
             }
